fad8e7803fe8c8504afa27359f7865aed45fc170
Shreyash_Raut <67021340+shreyash-rau@users.noreply.github.com>
2024-04-13 07:08:18 +0530

    Update BookDetails.jsx

--- src/BookDetails.jsx
+++ src/BookDetails.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const BookDetails = ({ book }) => {
+  const navigate = useNavigate();
+  const [showAll, setShowAll] = useState(false);
+
   if (!book) {
-    return <div>Select a book to see the details</div>;
+    return <div className='details'>Select a book to see the details</div>;
   }
+
+  const handleBuyNow = () => {
+    navigate(`/buy/${book.id}`);
+    return alert('Ready to Deliver Your Place')
+  };
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
+  const description = showAll ? book.volumeInfo.description : `${book.volumeInfo.description.substring(0, 300)}...`;
 
   return (
     <div className="book-details">
       <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
       <h2>{book.volumeInfo.title}</h2>
       <p>{book.volumeInfo.authors.join(', ')}</p>
-      <p>{book.volumeInfo.description}</p>
+      <p>{description}</p>
+      <button onClick={toggleShowAll}>{showAll ? 'Show Less' : 'Show More'}</button>
+      <button onClick={() => alert('Added to cart')}>Add to Cart</button>
+      <button onClick={handleBuyNow}>Buy Now</button>
     </div>
   );
 };
